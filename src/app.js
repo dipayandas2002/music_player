0350a8d06063f23f9ae4939fc187dd89be6e1c25
@dipayandas2002 <dipayandas360@gmail.com>
2021-08-05 11:13:13 +0530

    done

--- src/app.js
+++ src/app.js
@@ -1,17 +1,74 @@
-import React from "react";
+import React, { useState } from "react";
 import Search from "../src/components/search";
-import Playlist from "../src/components/playlist";
-import Player from "../src/components/player";
+// import Playlist from "../src/components/playlist";
+// import Player from "../src/components/player";
+import { } from "../src/CSS/search.css";
 
-import {} from "../src/CSS/search.css";
+
+function List(props) {
+
+  function handleAdd() {
+    props.onAdd(props.id)
+  }
+
+  return <li >
+    <div>{props.name}</div>
+    <button id={props.id} onClick={handleAdd}>+</button>
+  </li>
+
+}
 
 function App() {
+  const [searcharray, arrayChange] = useState([]);
+  const [playlistArray, playArrayChange] = useState([]);
+
+  
+  function updateArray(array) {
+    // console.log(array);
+    arrayChange(() => {
+      return array;
+    })
+  }
+
+  function addHandler(index) {
+
+    playArrayChange((prevValue) => {
+      return [...prevValue, searcharray[index].snippet.title]
+      
+    })
+  }
+
+
   return (
-    <div>
+    <div style={{ display: "flex" }}>
       <div className="search">
-        <Search />
+        <Search searchFunction={updateArray} />
+        <ul>
+          {searcharray.map((elements, index) => {
+            return <List
+              id={index}
+              key={index}
+              name={elements.snippet.title}
+              onAdd={addHandler}
+
+            />
+          })}
+        </ul>
+
+
       </div>
-      <div className="playlist"></div>
+      <div className="playlist">
+        <h1>Playlist</h1>
+        <ul>
+          {playlistArray.map((elements, index) => {
+            return <li
+              id={index}
+              key={index}
+            >{elements}</li>
+          })}
+
+        </ul>
+      </div>
       <div className="player"></div>
     </div>
   );
--- src/components/search.jsx
+++ src/components/search.jsx
@@ -1,20 +1,47 @@
-import React from "react";
-import {Button ,Form ,FormControl} from "react-bootstrap";
+import React,{useState} from "react";
+import { Button, Form, FormControl } from "react-bootstrap";
 
 
-function search() {
-  return (
+
+function Search(props) { //S of search must be in capital letter
+
+  const [input, inputChange] = useState("")  //in useState(`here lies the primary value`)
+  // let array=[]
+
+  function changeHandler(event) {
+    const val = event.target.value;
+    inputChange(val);        
     
-    <Form className="d-flex">
-      <FormControl
+    //change the value here // return when the input is of type array or object
+    // console.log(val);
+  }
+
+  function clickHandler() {
+    fetch(`https://youtube.googleapis.com/youtube/v3/search?part=snippet&maxResults=10&q=${input}&key=AIzaSyAQDDv-XUWs9qtI8nSRTNguc8oAQiT9frw`)
+  .then(response => response.json())
+  .then(data => {
+    // console.log(data.items); //data.items[].snippet.title or thumbnails{} // items is an []
+    
+    props.searchFunction(data.items);
+    
+  });
+  inputChange("")
+  }
+
+
+  return (
+
+    <Form className="d-flex" >
+      <FormControl onChange={changeHandler}
         type="search"
         placeholder="Search"
         aria-label="Search"
+        value={input}
       />
-      <Button variant="outline-primary">Search</Button>
+      <Button onClick={clickHandler} variant="outline-primary">Search</Button>
     </Form>
-    
+
   );
 }
 
-export default search;
+export default Search;
